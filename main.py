ebc9958f043ba7516b19e10a2e1dad3af6be4311
Екатерина <ekaterina.s3006@yandex.ru>
2023-10-27 09:32:09 +0300

    add functions

--- main.py
+++ main.py
@@ -1,16 +1,60 @@
-# This is a sample Python script.
+import psycopg2
+import requests
+from DBManager import DBManager
+from config import config
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+empls_list = [
+    1122462,
+    3529,
+    4934,
+    999442,
+    5088268,
+    5382804,
+    4307094,
+    11463,
+    1517303
+]
+# 'https://api.hh.ru/employers/{employer_id}'
+# url = 'https://api.hh.ru/employers/1122462'
+# headers = {'User-Agent': 'api-test-agent'}
+# r = requests.get(url=url, headers=headers)
+# responce = r.json()
+# print(responce['id'])
+# print(responce['name'])
+# print(responce['open_vacancies'])
+class APIManager():
+    url = 'https://api.hh.ru/employers/'
+    headers = {'User-Agent': 'api-test-agent'}
+    def __init__(self, empls_list):
+        self.empls_list = empls_list
+    @staticmethod
+    def setup_connection():
+        params = config()
+        try:
+            with psycopg2.connect(**params) as conn:
+                conn.autocommit = True
+                return conn
+        except psycopg2.Error as e:
+            print(f"PostgreSQL error: {e}")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
+    def get_employers(self, id: int):
+        """Получает вакансии"""
+        r = requests.get(self.url + str(id), headers=self.headers)
+        return r.json()
+
+    def save_employers_info(self):
+        conn = self.setup_connection()
+        with conn.cursor() as cur:
+            for i in self.empls_list:
+                responce = self.get_employers(i)
+                cur.execute(
+                    'INSERT INTO employees (employee_id, company_name, open_vacancies)'
+                    'VALUES (%s, %s, %s)',
+                    [int(responce["id"]), responce["name"], int(responce["open_vacancies"])]
+                )
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+apimanager = APIManager(empls_list)
+apimanager.save_employers_info()
