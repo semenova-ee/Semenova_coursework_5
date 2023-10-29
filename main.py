994a4f3ff69b8aca392e38534b00f147fa629653
Екатерина <ekaterina.s3006@yandex.ru>
2023-10-29 11:00:39 +0300

    change class APIManager

--- main.py
+++ main.py
@@ -23,7 +23,9 @@ empls_list = [
 # print(responce['name'])
 # print(responce['open_vacancies'])
 class APIManager():
-    url = 'https://api.hh.ru/employers/'
+    url_employers = 'https://api.hh.ru/employers/'
+    url_vacancy = 'https://api.hh.ru/vacancies/'
+    url_list_of_vacancies = 'https://api.hh.ru/vacancies?employer_id='
     headers = {'User-Agent': 'api-test-agent'}
     def __init__(self, empls_list):
         self.empls_list = empls_list
@@ -39,21 +41,61 @@ class APIManager():
         except Exception as e:
             print(f"An error occurred: {e}")
 
-    def get_employers(self, id: int):
+    def send_request(self, id: int, for_vacancies: bool = False, for_current_vacancy: bool = False):
         """Получает вакансии"""
-        r = requests.get(self.url + str(id), headers=self.headers)
+        if for_vacancies:
+            r = requests.get(self.url_list_of_vacancies + str(id), headers=self.headers)
+            return r.json()
+        elif for_current_vacancy:
+            r = requests.get(self.url_vacancy + str(id), headers=self.headers)
+            return r.json()
+        r = requests.get(self.url_employers + str(id), headers=self.headers)
         return r.json()
 
     def save_employers_info(self):
         conn = self.setup_connection()
         with conn.cursor() as cur:
             for i in self.empls_list:
-                responce = self.get_employers(i)
+                responce = self.send_request(i)
                 cur.execute(
                     'INSERT INTO employees (employee_id, company_name, open_vacancies)'
                     'VALUES (%s, %s, %s)',
                     [int(responce["id"]), responce["name"], int(responce["open_vacancies"])]
                 )
+
+    def save_vacancy_info(self):
+        conn = self.setup_connection()
+        with conn.cursor() as cur:
+            for i in self.empls_list:
+                responce = self.send_request(i, True)
+                items = responce['items']
+                for item in items:
+                    res = self.send_request(item['id'], for_current_vacancy = True)
+                    salary = self.inspect_salary(res)
+                    if salary:
+                        cur.execute(
+                        'INSERT INTO vacancies (employee_id, company_name, open_vacancies)'
+                        'VALUES (%s, %s, %s)',
+                        [int(responce["id"]), responce["name"], int(responce["open_vacancies"])]
+                        )
+
+    @staticmethod
+    def inspect_salary(vacancy: dict) -> int or None:
+        try:
+            if vacancy['salary']['currency'] != "RUR":
+                return None
+            from_salary = vacancy["salary"]["from"]
+            to_salary = vacancy["salary"]["to"]
+            if vacancy["salary"] is None:
+                return None
+            elif from_salary and to_salary:
+                return int(from_salary)
+            elif from_salary:
+                return int(from_salary)
+            else:
+                return None
+        except Exception:
+            return None
 
 
 apimanager = APIManager(empls_list)
